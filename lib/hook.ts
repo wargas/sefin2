cf722d2473312c615621d87a75c62690dccefc6a
Wargas Teixeira <wargasteixeira@192.168.0.19>
2026-05-11 19:47:48 -0300

    teto-stf

--- lib/hook.ts
+++ lib/hook.ts
@@ -3,180 +3,216 @@ import { Receita } from "@/types";
 import { filter, sumBy } from "lodash";
 import { useCallback, useMemo, useState } from "react";
 
-
-
 export function useHook() {
-    const [outrasReceitas, setOutrasReceitas] = useState<Receita[]>([])
+  const [outrasReceitas, setOutrasReceitas] = useState<Receita[]>([]);
 
-    const [params, setParams] = useState({
-        dependentesIR: 0,
-        nivel: '1A',
-        dependentesSF: 0,
-        gdf: 100,
-        ita: 0.15,
-        complementar: 8.5,
-        sindicato: 240,
-        cargo: 'AUDITOR',
-        fidaf: 0,
-        rav: 800,
+  const [params, setParams] = useState({
+    dependentesIR: 0,
+    nivel: "1A",
+    dependentesSF: 0,
+    gdf: 100,
+    ita: 0.15,
+    complementar: 8.5,
+    sindicato: 240,
+    cargo: "AUDITOR",
+    fidaf: 0,
+    rav: 800,
+    saude: true,
+    teto: true,
+    ajuste: 0,
+    consignado: 0,
+    diasTrabalhados: 30,
+  });
+
+  const ajuste = useMemo(() => 1 + params.ajuste / 100, [params]);
+  const tetoPrefeito = config.remuneracaoPrefeito * ajuste;
+
+  const teto = params.teto ? tetoPrefeito : config.remuneracaoSTF;
+
+  const receitas = useMemo(() => {
+    const cargo = params.cargo as "AUDITOR" | "ANALISTA";
+
+    const nivel = params.nivel as "1A";
+    const vencimento =
+      config.tabela[cargo][nivel] * config.ajustes["2026-01-01"] * ajuste;
+
+    const baseGdf =
+      config.tabela[cargo]["4A"] * config.ajustes["2026-01-01"] * 0.4 * ajuste;
+
+    const coeficienteTrabalhado = params.diasTrabalhados / 30;
+
+    return [
+      {
+        name: "vencimento",
+        ir: true,
         saude: true,
+        previdencia: true,
         teto: true,
-        ajuste: 0,
-        consignado: 0,
-        diasTrabalhados: 30
-    })
+        value: vencimento * coeficienteTrabalhado,
+      },
+      {
+        name: "ita",
+        ir: true,
+        saude: true,
+        previdencia: true,
+        teto: true,
+        value: vencimento * params.ita * coeficienteTrabalhado,
+      },
+      {
+        name: "gdf",
+        ir: true,
+        saude: true,
+        previdencia: true,
+        teto: true,
+        value: ((baseGdf * params.gdf) / 100) * coeficienteTrabalhado,
+      },
+      {
+        name: "RAV",
+        ir: true,
+        saude: true,
+        previdencia: true,
+        teto: true,
+        value: params.rav * config.valorPontoRAV * coeficienteTrabalhado,
+      },
+      {
+        name: "salario-familia",
+        ir: false,
+        saude: false,
+        previdencia: false,
+        teto: false,
+        value: 8.13 * params.dependentesSF,
+      },
+      {
+        name: "combustivel",
+        ir: false,
+        saude: false,
+        previdencia: false,
+        teto: false,
+        value:
+          (params.cargo == "AUDITOR" ? 1652.45 : 0) * coeficienteTrabalhado,
+      },
+      {
+        name: "fidaf",
+        ir: true,
+        saude: false,
+        previdencia: false,
+        teto: false,
+        value: params.fidaf,
+      },
+      ...outrasReceitas,
+    ];
+  }, [params, config, outrasReceitas]);
 
-    const ajuste = useMemo(() => 1 + (params.ajuste / 100), [params]) 
-    const tetoPrefeito = config.remuneracaoPrefeito * ajuste;
+  const bcSaude = useMemo(() => {
+    let geral_teto = sumBy(
+      filter(receitas, { saude: true, teto: true }),
+      "value",
+    );
+    let geral_no_teto = sumBy(
+      filter(receitas, { saude: true, teto: false }),
+      "value",
+    );
 
-    const receitas = useMemo(() => {
+    if (geral_teto > teto && params.teto) {
+      geral_teto = teto;
+    }
 
-        const cargo = params.cargo as "AUDITOR" | "ANALISTA";
+    return geral_teto + geral_no_teto;
+  }, [receitas]);
 
-        const nivel = params.nivel as "1A"
-        const vencimento = config.tabela[cargo][nivel] * config.ajustes["2026-01-01"] * ajuste
+  const bcPrevidencia = useMemo(() => {
+    let geral_teto = sumBy(
+      filter(receitas, { previdencia: true, teto: true }),
+      "value",
+    );
+    let geral_no_teto = sumBy(
+      filter(receitas, { previdencia: true, teto: false }),
+      "value",
+    );
 
-        const baseGdf = config.tabela[cargo]["4A"] * config.ajustes["2026-01-01"] * 0.4 * ajuste
+    if (geral_teto > teto && params.teto) {
+      geral_teto = teto;
+    }
 
-        const coeficienteTrabalhado = params.diasTrabalhados / 30
+    const geral = geral_teto + geral_no_teto;
 
-        return [
-            {
-                name: 'vencimento',
-                ir: true,
-                saude: true,
-                previdencia: true,
-                teto: true,
-                value: vencimento * coeficienteTrabalhado
-            },
-            {
-                name: 'ita',
-                ir: true,
-                saude: true,
-                previdencia: true,
-                teto: true,
-                value: vencimento * params.ita * coeficienteTrabalhado
-            },
-            {
-                name: 'gdf',
-                ir: true,
-                saude: true,
-                previdencia: true,
-                teto: true,
-                value: (baseGdf * params.gdf / 100)  * coeficienteTrabalhado
-            },
-            {
-                name: 'RAV',
-                ir: true,
-                saude: true,
-                previdencia: true,
-                teto: true,
-                value: (params.rav * config.valorPontoRAV) * coeficienteTrabalhado
-            },
-            {
-                name: 'salario-familia',
-                ir: false,
-                saude: false,
-                previdencia: false,
-                teto: false,
-                value: 8.13 * params.dependentesSF
-            },
-            {
-                name: 'combustivel',
-                ir: false,
-                saude: false,
-                previdencia: false,
-                teto: false,
-                value: (params.cargo == 'AUDITOR' ? 1652.45  : 0) * coeficienteTrabalhado
-            },
-            {
-                name: 'fidaf',
-                ir: true,
-                saude: false,
-                previdencia: false,
-                teto: false,
-                value: params.fidaf
-            },
-            ...outrasReceitas
-        ]
-    }, [params, config, outrasReceitas])
+    const bcPrevifor = config.tetoINSS;
+    const bcComplementar = geral - bcPrevifor;
 
-    const bcSaude = useMemo(() => {
-        let geral_teto = sumBy(filter(receitas, { saude: true, teto: true }), 'value')
-        let geral_no_teto = sumBy(filter(receitas, { saude: true, teto: false }), 'value')
+    return {
+      previdor: bcPrevifor,
+      complementar: bcComplementar,
+    };
+  }, [receitas]);
 
-        if (geral_teto > tetoPrefeito && params.teto) {
-            geral_teto = tetoPrefeito
-        }
+  const bcIR = useMemo(() => {
+    const descontoDependente = params.dependentesIR * 189.59;
 
-        return geral_teto + geral_no_teto;
-    }, [receitas])
+    let ir_teto = sumBy(filter(receitas, { ir: true, teto: true }), "value");
+    const ir_no_teto = sumBy(
+      filter(receitas, { ir: true, teto: false }),
+      "value",
+    );
 
-    const bcPrevidencia = useMemo(() => {
-        let geral_teto = sumBy(filter(receitas, { previdencia: true, teto: true }), 'value')
-        let geral_no_teto = sumBy(filter(receitas, { previdencia: true, teto: false }), 'value')
+    if (ir_teto > teto && params.teto) {
+      ir_teto = teto;
+    }
 
-        if (geral_teto > tetoPrefeito && params.teto) {
-            geral_teto = tetoPrefeito
-        }
+    return ir_teto + ir_no_teto - descontoDependente;
+  }, [receitas, params]);
 
-        const geral = geral_teto + geral_no_teto;
+  const descontos = useMemo(() => {
+    const valorPrevidor = bcPrevidencia.previdor * 0.14;
+    const valorCeprev =
+      bcPrevidencia.complementar * (params.complementar / 100);
 
-        const bcPrevifor = config.tetoINSS;
-        const bcComplementar = geral - bcPrevifor
+    const bcIR2 = bcIR - valorPrevidor - valorCeprev;
 
-        return {
-            previdor: bcPrevifor,
-            complementar: bcComplementar
-        }
+    const valorIR = bcIR2 * 0.275 - 908.73;
 
-    }, [receitas])
+    const receitasTeto = sumBy(filter(receitas, { teto: true }), "value");
 
-    const bcIR = useMemo(() => {
-        const descontoDependente = params.dependentesIR * 189.59
+    const descontoTeto =
+      teto < receitasTeto
+        ? receitasTeto - teto
+        : 0;
 
-        let ir_teto = sumBy(filter(receitas, { ir: true, teto: true }), 'value')
-        const ir_no_teto = sumBy(filter(receitas, { ir: true, teto: false }), 'value')
+    return [
+      { name: "saude", value: params.saude ? bcSaude * 0.02 : 0 },
+      { name: "previfor", value: valorPrevidor },
+      { name: "ceprev", value: valorCeprev },
+      { name: "sindicato", value: params.sindicato },
+      { name: "irpf", value: valorIR },
+      { name: "desconto teto", value: descontoTeto },
+      { name: "consignados", value: params.consignado },
+    ];
+  }, [receitas, params]);
 
-        if (ir_teto > tetoPrefeito && params.teto) {
-            ir_teto = tetoPrefeito
-        }
+  const changeValue = useCallback(
+    (data: Partial<typeof params>) => {
+      setParams((p) => ({ ...p, ...data }));
+    },
+    [params],
+  );
 
-        return ir_teto + ir_no_teto - descontoDependente
-    }, [receitas, params])
+  const removeOutrasReceitasById = useCallback(
+    (id: string) => {
+      setOutrasReceitas((old) => old.filter((o) => o.id != id));
+    },
+    [outrasReceitas],
+  );
 
-    const descontos = useMemo(() => {
- 
-        const valorPrevidor = bcPrevidencia.previdor * 0.14
-        const valorCeprev = bcPrevidencia.complementar * (params.complementar / 100)
-
-        const bcIR2 = bcIR - valorPrevidor - valorCeprev;
-
-        const valorIR = bcIR2 * 0.275 - 908.73
-
-        const receitasTeto = sumBy(filter(receitas, { teto: true }), 'value')
-
-        const descontoTeto = (tetoPrefeito < receitasTeto && params.teto) ? receitasTeto - tetoPrefeito : 0
-
-        return [
-            { name: 'saude', value: params.saude ? bcSaude * 0.02 : 0 },
-            { name: 'previfor', value: valorPrevidor },
-            { name: 'ceprev', value: valorCeprev },
-            { name: 'sindicato', value: params.sindicato },
-            { name: 'irpf', value: valorIR },
-            { name: 'desconto teto', value: descontoTeto },
-            { name: 'consignados', value: params.consignado }
-        ]
-    }, [receitas, params])
-
-    const changeValue = useCallback((data: Partial<typeof params>) => {
-        setParams(p => ({ ...p, ...data }))
-    }, [params])
-
-
-    const removeOutrasReceitasById = useCallback((id: string) => {
-        setOutrasReceitas(old => old.filter(o => o.id != id))
-    }, [outrasReceitas])
-
-    return { changeValue, removeOutrasReceitasById, descontos, bcIR, bcPrevidencia, bcSaude, receitas, params, setParams, outrasReceitas, setOutrasReceitas }
+  return {
+    changeValue,
+    removeOutrasReceitasById,
+    descontos,
+    bcIR,
+    bcPrevidencia,
+    bcSaude,
+    receitas,
+    params,
+    setParams,
+    outrasReceitas,
+    setOutrasReceitas,
+  };
 }
